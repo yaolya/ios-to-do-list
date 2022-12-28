830b6929ca103c6dd9c14796436c9b6ae08f65de
yaolya <galyagina.olya@gmail.com>
2022-12-28 04:05:51 +0300

    Initial Commit

---
+++ ios-to-do-list/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-to-do-list
+//
+//  Created by Оля Галягина on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
